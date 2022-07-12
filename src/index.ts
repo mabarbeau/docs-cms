9a125cccf0392aa98ca8c3cd809eb4e2b421a3a2
Marc Barbeau <3001781-mabarbeau1@users.noreply.gitlab.com>
2022-07-12 10:39:50 -0700

    Remove title

--- src/index.ts
+++ src/index.ts
@@ -10,101 +10,90 @@ const {
 
 const app = express()
 
-const documentId = '1MJ85BbYgjurQ6AyCaPpdaTHSgalzVouuBybJf5O0uos'
+const documentId = "1Bi9BMSDxPJSsAe9xsmcwq4GIoNO6GQMXQc_Pwpudhdo";
 
-app.listen(APP_PORT, APP_HOST)
+app.listen(APP_PORT, APP_HOST);
 
-console.log(`Running on http://${APP_HOST}:${APP_PORT}`)
+console.log(`Running on http://${APP_HOST}:${APP_PORT}`);
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/documents.readonly', 'https://www.googleapis.com/auth/drive.readonly']
+const SCOPES = [
+  "https://www.googleapis.com/auth/documents.readonly",
+  "https://www.googleapis.com/auth/drive.readonly",
+];
 
 function getClient() {
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URI,
-  )
+    process.env.GOOGLE_REDIRECT_URI
+  );
 }
 
 function generateAuthUrl() {
-  const oAuth2Client = getClient()
+  const oAuth2Client = getClient();
 
   const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
+    access_type: "offline",
     scope: SCOPES,
-  })
-  return `<a target="__blank" href='${authUrl}'>${authUrl}</a>`
+  });
+  return `<a target="__blank" href='${authUrl}'>${authUrl}</a>`;
 }
 
 function getNewToken(code) {
   return new Promise((resolve, reject) => {
-    const oAuth2Client = getClient()
+    const oAuth2Client = getClient();
 
     oAuth2Client.getToken(code, (err, token) => {
-      if (err) reject(err)
-      if (!token.access_token) reject(token)
-      resolve(Token.insert({
-        documentId,
-        token,
-      }))
-    })
-  })
+      if (err) reject(err);
+      if (!token.access_token) reject(token);
+      resolve(
+        Token.insert({
+          documentId,
+          token,
+        })
+      );
+    });
+  });
 }
 
 function authorize(token) {
-  const oAuth2Client = getClient()
-  oAuth2Client.setCredentials(token)
-  return oAuth2Client
-}
-
-function printDocTitle(auth) {
-  return new Promise((resolve, reject) => {
-    const docs = google.docs({ version: 'v1', auth })
-
-    docs.documents.get({
-      documentId,
-    }, (err, res) => {
-      if (err) reject(err)
-      if (!res?.data?.title) reject(res)
-      resolve(res.data.title)
-    })
-  })
+  const oAuth2Client = getClient();
+  oAuth2Client.setCredentials(token);
+  return oAuth2Client;
 }
 
 async function getDocument(auth) {
-  const drive = google.drive({ version: 'v3', auth })
+  const drive = google.drive({ version: "v3", auth });
 
-  return drive.files.export(
-    {
+  return drive.files
+    .export({
       fileId: documentId,
-      mimeType: 'text/html',
-    },
-  ).then(({ data }) => data)
+      mimeType: "text/html",
+    })
+    .then(({ data }) => data);
 }
 
-app.get('/', async (_, res) => {
-  res.send((await Page.first({ documentId })).attributes.html)
-})
+app.get("/", async (_, res) => {
+  res.send((await Page.first({ documentId })).attributes.html);
+});
 
-app.get('/admin', async (req, res) => {
-  const token = (await Token.first({ documentId }))?.attributes?.token
+app.get("/admin", async (req, res) => {
+  const token = (await Token.first({ documentId }))?.attributes?.token;
 
   if (!token) {
     if (req.query.code) {
-      await getNewToken(req.query.code)
+      await getNewToken(req.query.code);
     } else {
-      res.send(generateAuthUrl())
+      res.send(generateAuthUrl());
     }
   }
 
-  const client = authorize(token)
+  const client = authorize(token);
 
-  const title = await printDocTitle(client)
+  const html = await getDocument(client);
 
-  const html = await getDocument(client)
+  await Page.insert({ documentId, html });
 
-  await Page.update({ documentId }, { documentId, title, html })
-
-  res.send(html)
-})
+  res.send(html);
+});
